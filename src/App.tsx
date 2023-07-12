66bcacd3981b94267af4b14b03e80bd6d77364c4
Laura Hall <podinjshmi1993@gmail.com>
2023-07-12 13:56:04 +0100

    added saved doggy photos

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,25 @@ interface DogPhoto {
 }
 
 function App() {
-  const [dogPhoto, setDogPhoto] = useState<DogPhoto>();
+  const [dogPhoto, setDogPhoto] = useState<DogPhoto | undefined>();;
+  const [storeDogPhoto, setStoreDogPhoto] = useState<DogPhoto[]>([]);
 
   const handleGetDogPhoto= async () => {
     const response = await fetch(
-      "https://dog.ceo/api/breeds/image/random"
-    );
+      "https://dog.ceo/api/breeds/image/random");
     const jsonBody = await response.json();
     setDogPhoto(jsonBody);
   };
-
-  // const handleGetJoke = () => {
-  //   fetch("https://jokestemp.neillbogie.repl.co/jokes/general/random")
-  //     .then((response) => response.json())
-  //     .then((jsonBody: Joke[]) => setJoke(jsonBody[0]));
-  // };
+ 
+  const handleStoreDogPhoto = () => {
+    if(dogPhoto){
+      setStoreDogPhoto([
+      ...storeDogPhoto,
+       dogPhoto,
+    ]);
+    }
+    
+  };
 
   if (dogPhoto) {
     return (
@@ -30,6 +34,14 @@ function App() {
          <img src={dogPhoto.message} alt="Cute Doggy"/>
         <hr />
         <button onClick={handleGetDogPhoto}>Get another Doggy!</button>
+        <br />
+        <button onClick={handleStoreDogPhoto}>Save Doggy Pic!</button>
+        <br />
+        <br />
+        <h2>Saved Doggy Pics</h2>
+        {storeDogPhoto.map((photo, index) => (
+          <img src={photo.message} key={index} alt="saved daggy"/>
+        ))}
       </div>
     );
   } else {
@@ -40,7 +52,9 @@ function App() {
           Click the button to trigger a <code>fetch</code> that gets a random
           Dog Picture from an API!
         </p>
-        <button onClick={handleGetDogPhoto}>Get joke</button>
+        <button onClick={handleGetDogPhoto}>Get Doggy!</button>
+        <br />
+        <button onClick={handleStoreDogPhoto}>Save Doggy Pic!</button>
       </div>
     );
   }
